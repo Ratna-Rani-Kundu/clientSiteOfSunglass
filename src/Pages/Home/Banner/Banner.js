31ec50e85f2c157cc7e2eca4cd3d1abc106f7306
Ratna-Rani-Kundu <melaashopbd@gmail.com>
2021-11-14 09:24:09 +0600

    banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,7 +3,12 @@ import "./Banner.css"
 const Banner = () => {
     return (
         <div className="banner">
-            <h1>hello</h1>
+       
+            
+        <h1 className="p-4 ">Welcome To <span className="text-warning p-4 " >JEWEL-MART</span></h1> 
+        <h3  >Premium Quality</h3>
+        <h1 className="p-4 ">Pure Ornaments</h1>
+           
         </div>
     );
 };
